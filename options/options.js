d0399e723ea1ffd3def882a59957de01a338923d
Crowdbotics <team@crowdbotics.com>
2024-02-29 22:14:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-moon-46731",
+  url: "https://fragrant-moon-46731.botics.co",
+  api: "https://fragrant-moon-46731.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
